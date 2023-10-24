32d26e1d29e65a7dbc848f5ac4c200cb9e67732c
wheatgreaser <108815657+wheatgreaser@users.noreply.github.com>
2023-10-25 04:54:37 +0530

    Add files via upload

---
+++ housepricepredictinator.py
@@ -0,0 +1,20 @@
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("data.csv")
+
+Y = df[["SalePrice"]]
+Y = Y.head(100)
+
+X = df[["LotArea", "OverallCond", "YearBuilt"]]
+X = X.head(100)
+
+reg = linear_model.LinearRegression()
+reg.fit(X, Y)
+
+print(reg.predict([[8450, 7, 2002]]))
+
+
+
+
+
